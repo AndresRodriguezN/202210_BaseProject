0d34521c38dee4cfca6367178c18a95025398823
AndresRodriguezN <af.rodriguezn@uniandes.edu.co>
2022-04-30 20:38:46 -0500

    Servicio creado para obtener un conjunto de datos de cafes

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
 
 
 @Component({
@@ -11,9 +12,16 @@ export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
 
-  constructor() { }
+  constructor(private cafeService: CafeService) { }
+
+  getCafes(): void {
+    this.cafeService.getCafes().subscribe((cafes) => {
+      this.cafes = cafes;
+    });
+  }
 
   ngOnInit() {
+    this.getCafes();
   }
 
 }
